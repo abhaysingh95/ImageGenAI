5c402546f119988db2cc16d036d941030c36f560
Abhay Singh <128255771+abhaysingh95@users.noreply.github.com>
2025-08-28 18:37:11 +0530

    Update mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -5,10 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on('connected', ()=>{
         console.log('MongoDB connected successfully');
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
 }
 
 export default connectDB;
